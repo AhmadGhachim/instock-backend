5c2062533180395bbefefe654e03e58f08968842
AhmadGhachim <60991291+AhmadGhachim@users.noreply.github.com>
2025-02-04 12:26:54 -0600

    Basic setup for an express server, routes and controllers files

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the In-Stock API Backend!");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
+
+
+
