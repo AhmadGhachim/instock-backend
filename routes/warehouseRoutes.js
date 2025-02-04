399c91e2fdfdbc388b2e9b08e86f41ee67e6d433
AhmadGhachim <60991291+AhmadGhachim@users.noreply.github.com>
2025-02-04 13:56:01 -0600

    Completed and tested API endpoint to get warehouses by id

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.route('/').get(warehouseController.getWarehouses)
 
+router.route('/:id').get(warehouseController.getWarehouseById)
+
 export default router;
