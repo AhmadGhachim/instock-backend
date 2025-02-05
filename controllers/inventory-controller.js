ae7c89973ac0cc1a4e4701c3dae0442be40c1b2a
Your Name <you@example.com>
2025-02-05 14:41:10 -0800

    added get route set up to the controllers file

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -2,4 +2,11 @@ import initKnex from "knex";
 import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
-export {  };
+const getInventoryList = async (_req, res) => {
+  try {
+    const data = await knex("inventories");
+    res.status(200).json(data);
+  } catch (error) {}
+};
+
+export {};
