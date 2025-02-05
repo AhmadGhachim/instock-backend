806fd5848550c711079f672f07c09102cec1e792
Alma Nursultan <alma@Almas-MacBook-Air.local>
2025-02-05 12:50:01 -0700

    added POST/api/warehouse endpoint

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -3,8 +3,13 @@ import * as warehouseController from "../controllers/warehouse-controller.js";
 
 const router = express.Router();
 
-router.route('/').get(warehouseController.getWarehouses);
+router.route("/").get(warehouseController.getWarehouses);
 
-router.route('/:id').get(warehouseController.getWarehouseById).put(warehouseController.updateWarehouse);
+router
+  .route("/:id")
+  .get(warehouseController.getWarehouseById)
+  .put(warehouseController.updateWarehouse);
+
+router.route("/").post(warehouseController.createWarehouse);
 
 export default router;
