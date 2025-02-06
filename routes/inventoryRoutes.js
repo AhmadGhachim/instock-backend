d7b4b1a69088b7c61491fe528e3f7f95d429ad2e
Your Name <you@example.com>
2025-02-05 16:13:26 -0800

    added add item by id for inventory list

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -4,6 +4,7 @@ import * as inventoryController from "../controllers/inventory-controller.js";
 const router = express.Router();
 
 router.route("/").get(inventoryController.getInventoryList);
+router.route("/:id").get(inventoryController.getItemById);
 
 
 export default router;
